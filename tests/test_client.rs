ec0281151f8012e7bc4c383a9123949ec68ed3db
Tony Aldridge <tony@dabapps.com>
2014-10-07 13:34:40 +0100

    Stubbing out client tests

---
+++ tests/test_client.rs
@@ -0,0 +1,42 @@
+#[test]
+fn standard_connection() {
+    unimplemented!();
+}
+
+#[test]
+fn connection_rejected() {
+    unimplemented!();
+}
+
+#[test]
+fn connection_ignored() {
+    unimplemented!();
+}
+
+#[test]
+fn different_request_count() {
+    unimplemented!();
+}
+
+//TODO: Find a sensible way of testing timeout lengths
+
+#[test]
+fn empty_polling() {
+    unimplemented!();
+}
+
+fn single_item_polling() {
+    unimplemented!();
+}
+
+fn multiple_item_polling() {
+    unimplemented!();
+}
+
+fn disconnection() {
+    unimplemented!();
+}
+
+fn timeout() {
+    unimplemented!();
+}
