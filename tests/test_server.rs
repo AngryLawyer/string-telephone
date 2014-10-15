d2691e22b4f21796196d40a02069db39cf98fd9d
Tony Aldridge <tony@dabapps.com>
2014-10-15 13:55:13 +0100

    Basic Create server test

--- tests/test_server.rs
+++ tests/test_server.rs
@@ -1,14 +1,29 @@
 #![feature(macro_rules)]
 extern crate string_telephone;
 
+use string_telephone::{ConnectionConfig, ClientConnectionConfig, Server, Packet, PollEmpty, PacketConnect, PacketMessage, PacketDisconnect, PollDisconnected};
+use std::io::net::ip::{Ipv4Addr, SocketAddr};
+use std::time::duration::Duration;
+
 mod test_shared;
+
+fn generate_settings(port: u16, protocol_id: u32) -> (SocketAddr, ConnectionConfig<Vec<u8>>) {
+    let my_addr = SocketAddr{ ip: Ipv4Addr(127, 0, 0, 1), port: port };
+    let settings = ConnectionConfig::new(protocol_id, Duration::seconds(10), test_shared::deserializer, test_shared::serializer);
+    (my_addr, settings)
+}
 
 /**
  * Test we can start listening
  */
 #[test]
 fn create_server() {
-    unimplemented!()
+    let socket = 64000;
+    let (my_addr, settings) = generate_settings(socket, 121);
+    match Server::new(my_addr, settings) {
+        Ok(server) => (), //passed
+        Err(t) => fail!("Failed to create a server - {}", t)
+    };
 }
 
 /**
