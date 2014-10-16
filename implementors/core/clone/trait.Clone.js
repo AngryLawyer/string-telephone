63f5daf8cd6acc657853186ea21441c7b9c9eee5
Tony Aldridge <tony@dabapps.com>
2014-10-16 13:47:38 +0100

    Initial version of docs

---
+++ implementors/core/clone/trait.Clone.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['string_telephone'] = ["<a class='stability Unmarked' title='No stability level'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/clone/trait.Clone.html' title='core::clone::Clone'>Clone</a> for <a class='enum' href='string_telephone/packet/enum.PacketType.html' title='string_telephone::packet::PacketType'>PacketType</a>","<a class='stability Unmarked' title='No stability level'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/clone/trait.Clone.html' title='core::clone::Clone'>Clone</a> for <a class='struct' href='string_telephone/packet/struct.Packet.html' title='string_telephone::packet::Packet'>Packet</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ implementors/core/num/trait.FromPrimitive.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['string_telephone'] = ["<a class='stability Unmarked' title='No stability level'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/num/trait.FromPrimitive.html' title='core::num::FromPrimitive'>FromPrimitive</a> for <a class='enum' href='string_telephone/packet/enum.PacketType.html' title='string_telephone::packet::PacketType'>PacketType</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
